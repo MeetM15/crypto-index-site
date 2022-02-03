5e6f0666dcb473c47e63dc9233a500a610e162ae
PancakeSwapProto <lonecrypto777@gmail.com>
2022-02-03 08:05:44 -0800

    removed US

--- pages/index.js
+++ pages/index.js
@@ -845,24 +845,9 @@ export default function Home() {
               <div className="text-xs text-[#CCCDCE] w-full mt-8">
                 IMPORTANT LEGAL DISCLAIMER Access to products and services
                 detailed on this website may be restricted for certain persons
-                or countries.In particular, the products and services referred
-                to herein are not available to U.S.Persons, as defined by
-                Regulation S of the United States Securities and Exchange
-                Commission, as amended(“U.S.Persons”).The information contained
-                on this website is not available to U.S.Persons.Investors who
-                are such "U.S. Persons" should not view this website.The
-                provision of the information in this website does not constitute
-                an offer of securities to any person in the United States or to
-                any "U.S. Person." Index Block is not registered under the
-                U.S.Investment Company Act of 1940, as amended, nor is the sale
-                of INDB tokens registered under the U.S.Securities Act of 1933,
-                as amended.Consequently, it cannot be offered for sale or be
-                sold in the United States, its territories, possessions or
-                protectorates under its jurisdiction, nor to nationals, citizens
-                or residents in any of those areas, except pursuant to a valid
-                exemption.More generally, the products and services presented on
+                or countries. More generally, the products and services presented on
                 this website may only be purchased in jurisdictions in which
-                their marketing and distribution are authorised.Index Block
+                their marketing and distribution are authorised. Index Block
                 advises all interested parties to check in advance whether they
                 are legally entitled to purchase the products and / or services
                 presented on the website.
